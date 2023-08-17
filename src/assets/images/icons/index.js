9f7b996eb3fb65089fb6ab78a617939ab7e528dc
TusharSatam <tsatam91@gmail.com>
2023-08-17 14:16:44 +0530

    footer section ui

---
+++ src/assets/images/icons/index.js
@@ -0,0 +1,10 @@
+
+export { default as DownloadAppStore } from './DownloadAppStore.svg';
+export { default as DownloadPlayStore } from './DownloadPlayStore.svg';
+export { default as instagram } from './instagram.svg';
+export { default as facebook } from './facebook.svg';
+export { default as twitter } from './twitter.svg';
+export { default as linkedin } from './linkedin.svg';
+export { default as search } from './search.svg';
+export { default as Treato } from './Treato.svg';
+
--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -3,6 +3,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import { useDispatch, useSelector } from "react-redux";
 import SalonFilterModalDesktop from "../../components/_modals/filterSalon/SalonFilterModalDesktop/SalonFilterModalDesktop.js";
 import SalonFilterModalMobile from "../../components/_modals/filterSalon/SalonFilterModalMobile/SalonFilterModalMobile";
+import Footer from "../../components/Footer/Footer";
 export default function PageLayout({ children }) {
   const showModal = useSelector((state) => state.modal.showModal);
   const isMobileView = useSelector((state) => state.modal.isMobileView);
@@ -14,6 +15,7 @@ export default function PageLayout({ children }) {
       {showModal && isMobileView && <SalonFilterModalMobile />}
       {children}
       {/* Desktop Modal */}
+      <Footer/>
     </div>
   );
 }
