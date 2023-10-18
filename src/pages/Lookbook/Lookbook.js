b70f8ee09cfe8e2ccf121c4ffe90855462f905e0
TusharSatam <tsatam91@gmail.com>
2023-10-18 23:37:53 +0530

    added Lookbook desktop functionality

--- src/pages/Lookbook/Lookbook.js
+++ src/pages/Lookbook/Lookbook.js
@@ -2,6 +2,7 @@ import React from "react";
 import Masonry from "react-masonry-css";
 import styles from "./Lookbook.module.css";
 import ModalImage from "react-modal-image";
+
 import img1 from "../../assets/images/LookbookImages/Lookbook1.png";
 import img2 from "../../assets/images/LookbookImages/Lookbook2.png";
 import img3 from "../../assets/images/LookbookImages/Lookbook3.png";
@@ -21,29 +22,32 @@ import { useEffect } from "react";
 import { salon } from "../../services/salon";
 import { useSelector } from "react-redux";
 import { getAllServices } from "../../services/Services";
-const ImageWithDetails = ({ src, alt, rating }) => (
-  <div className={styles.imageContainer}>
-    <ModalImage
-      small={src}
-      large={src}
-      alt={alt}
-      className={styles.masonryImage}
-    />
-    <span className={styles.imageRating}>
-      <img src={starBlack} alt="starIcon" /> {rating}
-    </span>
-    <span className={styles.expandbutton}>
-      <img src={expandMoboImage} />
-    </span>
-    <p className={styles.imageText}>
-      A subtle salmon tinge on curled hairs. Golden long, waist length,
-      straight.
-    </p>
-  </div>
-);
+import { useRef } from "react";
 const Lookbook = () => {
+  const modalImageRef = useRef();
+  
+  const ImageWithDetails = ({ src, alt, rating }) => (
+    <div className={styles.imageContainer}>
+      <ModalImage
+        small={src}
+        large={src}
+        alt={alt}
+        className={styles.masonryImage}
+      />
+      <span className={styles.imageRating}>
+        <img src={starBlack} alt="starIcon" /> {rating}
+      </span>
+      {/* <span className={styles.expandbutton}>
+        <img src={expandMoboImage} />
+      </span> */}
+      <p className={styles.imageText}>
+        A subtle salmon tinge on curled hairs. Golden long, waist length,
+        straight.
+      </p>
+    </div>
+  );
   const salonsState = useSelector((state) => state.salons);
-  const [activeButton, setActiveButton] = useState("Hair");
+  const [activeButton, setActiveButton] = useState("All");
   const [locationInput, setLocationInput] = useState("");
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const [allSalonList, setallSalonList] = useState([]);
@@ -66,45 +70,186 @@ const Lookbook = () => {
   ];
   //lookbook image object
   const images = [
-    { src: img1, alt: "Image 1", rating: 4 },
-    { src: img2, alt: "Image 2", rating: 3 },
-    { src: img3, alt: "Image 3", rating: 5 },
-    { src: img4, alt: "Image 4", rating: 3.2 },
-    { src: img5, alt: "Image 5", rating: 3.4 },
-    { src: img6, alt: "Image 6", rating: 4.2 },
-    { src: img1, alt: "Image 7", rating: 4 },
-    { src: img2, alt: "Image 8", rating: 3 },
-    { src: img3, alt: "Image 9", rating: 5 },
-    { src: img4, alt: "Image 10", rating: 3.2 },
-    { src: img5, alt: "Image 11", rating: 3.4 },
-    { src: img6, alt: "Image 12", rating: 4.2 },
-    { src: img4, alt: "Image 13", rating: 3.2 },
-    { src: img5, alt: "Image 14", rating: 3.4 },
-    { src: img6, alt: "Image 15", rating: 4.2 },
-    { src: img1, alt: "Image 16", rating: 4 },
-    { src: img2, alt: "Image 17", rating: 3 },
-    { src: img3, alt: "Image 18", rating: 5 },
-    { src: img4, alt: "Image 19", rating: 3.2 },
-    { src: img5, alt: "Image 20", rating: 3.4 },
-    { src: img6, alt: "Image 21", rating: 4.2 },
-    { src: img4, alt: "Image 22", rating: 3.2 },
-    { src: img5, alt: "Image 23", rating: 3.4 },
-    { src: img6, alt: "Image 24", rating: 4.2 },
-  ];
-
-  // Define an object with button names as an array
-  const buttons = [
-    "Hair",
-    "Hair removal",
-    "Nail care",
-    "Facials&Skincare",
-    "Makeup",
+    {
+      src: img1,
+      alt: "Image 1",
+      rating: 4,
+      serviceType: "Hair",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img2,
+      alt: "Image 2",
+      rating: 3,
+      serviceType: "Hair Removal",
+      location: "Telangana, Hyderabada",
+    },
+    {
+      src: img3,
+      alt: "Image 3",
+      rating: 5,
+      serviceType: "Nail care",
+      location: "Telangana, Hyderabada",
+    },
+    {
+      src: img4,
+      alt: "Image 4",
+      rating: 3.2,
+      serviceType: "Hair Removal",
+      location: "Telangana, Hyderabada",
+    },
+    {
+      src: img5,
+      alt: "Image 5",
+      rating: 3.4,
+      serviceType: "Nail care",
+      location: "Telangana, Hyderabada",
+    },
+    {
+      src: img6,
+      alt: "Image 6",
+      rating: 4.2,
+      serviceType: "Spa",
+      location: "Telangana, Hyderabada",
+    },
+    {
+      src: img1,
+      alt: "Image 7",
+      rating: 4,
+      serviceType: "Hair Removal",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img2,
+      alt: "Image 8",
+      rating: 3,
+      serviceType: "Nail care",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img3,
+      alt: "Image 9",
+      rating: 5,
+      serviceType: "Hair",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img4,
+      alt: "Image 10",
+      rating: 3.2,
+      serviceType: "Makeup",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img5,
+      alt: "Image 11",
+      rating: 3.4,
+      serviceType: "Hair",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img6,
+      alt: "Image 12",
+      rating: 4.2,
+      serviceType: "Hair Removal",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img4,
+      alt: "Image 13",
+      rating: 3.2,
+      serviceType: "Nail care",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img5,
+      alt: "Image 14",
+      rating: 3.4,
+      serviceType: "Makeup",
+      location: "Agra, Uttar Pradesh",
+    },
+    {
+      src: img6,
+      alt: "Image 15",
+      rating: 4.2,
+      serviceType: "Spa",
+      location: "Agra, Uttar Pradesh",
+    },
+    {
+      src: img1,
+      alt: "Image 16",
+      rating: 4,
+      serviceType: "Hair",
+      location: "Agra, Uttar Pradesh",
+    },
+    {
+      src: img2,
+      alt: "Image 17",
+      rating: 3,
+      serviceType: "Hair Removal",
+      location: "Agra, Uttar Pradesh",
+    },
+    {
+      src: img3,
+      alt: "Image 18",
+      rating: 5,
+      serviceType: "Nail care",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img4,
+      alt: "Image 19",
+      rating: 3.2,
+      serviceType: "Makeup",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img5,
+      alt: "Image 20",
+      rating: 3.4,
+      serviceType: "Hair",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img6,
+      alt: "Image 21",
+      rating: 4.2,
+      serviceType: "Spa",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img4,
+      alt: "Image 22",
+      rating: 3.2,
+      serviceType: "Hair Removal",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img5,
+      alt: "Image 23",
+      rating: 3.4,
+      serviceType: "Nail care",
+      location: "Pune, Maharashtra",
+    },
+    {
+      src: img6,
+      alt: "Image 24",
+      rating: 4.2,
+      serviceType: "Hair",
+      location: "Pune, Maharashtra",
+    },
   ];
 
   // Function to handle button clicks and set the active button
   const handleButtonClick = (buttonName) => {
     setActiveButton(buttonName);
   };
+
+  useEffect(() => {
+    const filteredImages = filterImages();
+    setFilteredServiceData(filteredImages);
+  }, [activeButton]);
+
   // Function to handle input changes and set the locationInput state
   const handleLocationInputChange = (event) => {
     const inputValue = event.target.value;
@@ -132,6 +277,32 @@ const Lookbook = () => {
     1100: 2,
     700: 2,
   };
+  // Function to filter images based on locationInput and serviceType
+  const filterImages = () => {
+    let filteredImages = images; // Start with all images
+
+    if (locationInput != "") {
+      // Filter based on locationInput
+      filteredImages = filteredImages.filter((image) =>
+        image.location.toLowerCase().includes(locationInput.toLowerCase())
+      );
+    }
+
+    if (activeButton !== "All") {
+      // Filter based on the activeButton (serviceType)
+      filteredImages = filteredImages.filter(
+        (image) => image.serviceType === activeButton
+      );
+    }
+
+    return filteredImages;
+  };
+  const handleGoButtonClick = () => {
+    // Get the filtered images
+    const filteredImages = filterImages();
+    // Update the state with filtered images
+    setFilteredServiceData(filteredImages);
+  };
   useEffect(() => {
     const fetchSalons = async () => {
       try {
@@ -141,7 +312,6 @@ const Lookbook = () => {
           const { salons } = data; // Destructure 'salons' from 'data'
           setallSalonList(salons);
           setFilteredSalonData(salons);
-          console.log(salons);
         }
       } catch (error) {
         // Handle any errors here
@@ -154,21 +324,63 @@ const Lookbook = () => {
   useEffect(() => {
     // Call the getAllServices function when the component mounts
     async function fetchAllServices() {
-     try {
-       const { res, err } = await getAllServices();
- 
-       if (res) {
-         // If the request was successful, update the state with the data
-         setallServices(res?.data?.data); // Assuming the response data contains a "data" property
-       } 
-     } catch (error) {
-       // Handle unexpected errors here
-       setError(error);
-     }
-   }
- 
-   fetchAllServices();
- }, [])
+      try {
+        const { res, err } = await getAllServices();
+
+        if (res) {
+          // If the request was successful, update the state with the data
+          setallServices(res?.data?.data); // Assuming the response data contains a "data" property
+        }
+      } catch (error) {
+        // Handle unexpected errors here
+        console.log(error);
+      }
+    }
+
+    fetchAllServices();
+    setFilteredServiceData(images);
+    handleGoButtonClick();
+  }, []);
+
+  const masonryRef = useRef(null);
+  const applyCustomMargins = () => {
+    const masonryContainer = document.querySelector(".masonryContainer");
+    if (filteredServiceData?.length >= 5) {
+      // If images length is greater or equal to 5, add a class to the container
+      masonryContainer?.classList.add("images-length-greater-equal-5");
+    } else {
+      // If images length is less than 5, remove the class from the container
+      masonryContainer?.classList.remove("images-length-greater-equal-5");
+    }
+
+    if (filteredServiceData?.length >= 5) {
+      // Apply custom margins to specific elements when images.length is greater or equal to 5
+      const masonryDivs = document.querySelectorAll(".masonryContainer > div");
+      if (masonryDivs.length >= 1) {
+        masonryDivs[0].classList.add("masonry-div-1");
+      }
+      if (masonryDivs.length >= 2) {
+        masonryDivs[1].classList.add("masonry-div-2");
+      }
+      if (masonryDivs.length >= 4) {
+        masonryDivs[3].classList.add("masonry-div-4");
+      }
+    }
+  };
+
+  // Function to log the div elements inside Masonry
+  const applyIdsToMasonryDivs = () => {
+    const divs = document.querySelectorAll(".masonryContainer > div ");
+    divs.forEach((div, index) => {
+      div.classList.add(`masonry-div-${index + 1}`); // Add an id attribute
+    });
+  };
+
+  useEffect(() => {
+    applyIdsToMasonryDivs();
+    applyCustomMargins();
+  }, [filteredServiceData]);
+
   return (
     <div className={styles.lookbookContainer}>
       <h1>
@@ -193,6 +405,7 @@ const Lookbook = () => {
               setLocationInput("");
               setIsDropdownVisible(false);
               setFilteredSalonData(allSalonList);
+              setFilteredServiceData(images);
             }}
           />
         )}
@@ -200,6 +413,7 @@ const Lookbook = () => {
           className={`${styles.submitLocation} ${
             locationInput ? styles.blueBg : ""
           }`}
+          onClick={handleGoButtonClick}
         >
           GO
         </button>
@@ -219,29 +433,42 @@ const Lookbook = () => {
         )}
       </div>
       <div className={styles.serviceButtonWrapper}>
- 
-        {buttons.map((buttonName, index) => (
+        {allServices.map((service, index) => (
           <button
             key={index}
-            onClick={() => handleButtonClick(buttonName)}
+            onClick={() => handleButtonClick(service?.service_name)}
             className={
-              activeButton === buttonName ? `${styles.activeServiceType}` : ""
+              activeButton === service?.service_name
+                ? `${styles.activeServiceType}`
+                : ""
             }
           >
-            {buttonName}
+            {service?.service_name}
           </button>
         ))}
       </div>
-      <Masonry breakpointCols={breakpointColumnsObj} className={styles.masonry}>
-        {images.map((image, index) => (
-          <ImageWithDetails
-            key={index}
-            src={image.src}
-            alt={image.alt}
-            rating={image.rating}
-          />
-        ))}
+      {filteredServiceData.length === 0 && (
+        <div className={styles.zeroResponse}>
+          No results match the selected criteria
+        </div>
+      )}
+      <Masonry
+        id="masonry-container"
+        ref={masonryRef}
+        breakpointCols={breakpointColumnsObj}
+        className={`${styles.masonry} masonryContainer`}
+      >
+        {filteredServiceData?.length > 0 &&
+          filteredServiceData.map((image, index) => (
+            <ImageWithDetails
+              key={index}
+              src={image.src}
+              alt={image.alt}
+              rating={image.rating}
+            />
+          ))}
       </Masonry>
+  
     </div>
   );
 };
