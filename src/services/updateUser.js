fb534eaee8db7a148157add69e3f43280d5722d2
varsha <vharkal16@gmail.com>
2023-12-27 10:35:03 +0530

    task updated

--- src/services/updateUser.js
+++ src/services/updateUser.js
@@ -1,15 +1,20 @@
+import { toFormData } from 'axios';
 import axiosInstance from './axios'
-import { toFormData } from 'axios'
 
 /** Register the user after Verifying the OTP `POST: /api/auth/register/` */
 export const updateUser = async (userJWT,data) => {
    try {
+      console.log(data);
       const res = await axiosInstance.patch(`profile/update`,toFormData(data),{
-         headers: {
-         'token': userJWT
-         }
-       })
-      console.log("errrror", res);
+         
+         
+         method: "PATCH",
+            credentials: "include",
+            headers: {
+               'token': userJWT,
+              "Access-Control-Allow-Credentials": true,
+       }})
+      console.log( res);
       return { res: res, err: null }
    } catch (error) {
       return { err: error, res: null }
