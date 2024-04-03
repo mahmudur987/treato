73a2a6f877a7c0db76951ceedb82b60703552c41
mahmudur987 <safemahmud987@agmail.com>
2024-04-03 15:38:10 +0600

    manage holidays modal design complete

--- src/components/_modals/ManageHolyDays/ManageHolidays.js
+++ src/components/_modals/ManageHolyDays/ManageHolidays.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./ManageHolidays.module.css";
 import { IoMdArrowBack } from "react-icons/io";
-import { FaPlus } from "react-icons/fa6";
+import { FaAngleDown, FaArrowDown, FaPlus } from "react-icons/fa6";
 import ToggleButton from "../../Buttons/Toggle/ToggleButton";
 import { useSingleSalon } from "../../../services/salon";
 import { useGetHolidays } from "../../../services/holidays";
@@ -13,12 +13,18 @@ const formatDate = (dateString) => {
 };
 const ManageHolidays = ({ showModal, onClose }) => {
   const { data, isError, isLoading, error } = useGetHolidays();
+  const dateInputRef = useRef(null);
+  const [selectedDate, setSelectedDate] = useState("Fri,Mar 29");
+  const [show, setShow] = useState(false);
   const [isToggled, setIsToggled] = useState("");
   const handleToggle = (id) => {
     setIsToggled(id);
     console.log(id);
   };
-  console.log(isToggled);
+  const openDatePicker = () => {
+    dateInputRef.current.showPicker();
+  };
+
   return (
     <div className={`${styles.modal} ${showModal ? styles.show : ""}`}>
       <div className={styles.modalContent}>
@@ -34,12 +40,71 @@ const ManageHolidays = ({ showModal, onClose }) => {
           closed. You can update this list anytime before the relevant date.
         </p>
         <div className={styles.top}>
-          <button className={styles.addNewButton}>
-            <span>
-              <FaPlus />
-            </span>
-            <span>Add a new holiday</span>
-          </button>
+          {show ? (
+            <form className={styles.addHolidayForm}>
+              <div className={styles.topPart}>
+                <h4>Add a new holiday</h4>
+                <p className={styles.ToggleButtonWrapper}>
+                  <ToggleButton /> <span>close</span>
+                </p>
+              </div>
+              <div className={styles.bottomPart}>
+                <p
+                  className={styles.dateWrapper}
+                  style={{ position: "relative" }}
+                >
+                  <span style={{ fontWeight: "600" }}>Date</span>
+                  <span
+                    onClick={openDatePicker}
+                    style={{
+                      color: "#6D747A",
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "5px",
+                    }}
+                  >
+                    {selectedDate} <FaAngleDown />
+                  </span>
+                  <input
+                    ref={dateInputRef}
+                    type="date"
+                    style={{ display: "none", position: "absolute" }}
+                    onChange={(e) => {
+                      const selectedDate = e.target.value;
+                      const formattedDate = new Date(
+                        selectedDate
+                      ).toLocaleDateString("en-US", {
+                        weekday: "short",
+                        month: "short",
+                        day: "numeric",
+                      });
+                      setSelectedDate(formattedDate);
+                    }}
+                  />
+                </p>
+                <p className={styles.eventWrapper}>
+                  <span style={{ fontWeight: "600" }}>Event</span>
+                  <input type="text " placeholder="Enter holidays name" />
+                </p>
+                <button
+                  onClick={() => setShow(!show)}
+                  className={styles.addHolidaySave}
+                >
+                  save
+                </button>
+              </div>
+            </form>
+          ) : (
+            <button
+              onClick={() => setShow(!show)}
+              className={styles.addNewButton}
+            >
+              <span>
+                <FaPlus />
+              </span>
+              <span>Add a new holiday</span>
+            </button>
+          )}
 
           <div className={styles.row1}>
             <span
