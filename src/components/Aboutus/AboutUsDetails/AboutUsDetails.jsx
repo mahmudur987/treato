3961e4496215b5ea56bdcce7d646dd8c161a0814
Krishna-Kant-Patel <cu.17bcs1796@gmail.com>
2024-04-08 23:22:30 +0530

    :rocket:

--- src/components/Aboutus/AboutUsDetails/AboutUsDetails.jsx
+++ src/components/Aboutus/AboutUsDetails/AboutUsDetails.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import style from './AboutUsDetails.module.css';
 import aboutusImage from '../../../assets/images/AboutUs/aboutus.png'
-
+import image1 from '../../../assets/icons/Aboutus/image1.png';
+import image2 from '../../../assets/icons/Aboutus/image2.png';
+import Footer from '../../Footer/Footer';
 function AboutUsDetails() {
     return (
         <>
@@ -28,7 +30,43 @@ function AboutUsDetails() {
             </div>
             <div className={style.backeffect} ></div>
             <img src={aboutusImage} className={style.aboutimage} alt="" srcset="" />
-            <div className={style.OurTeamBox}>jskdhfkhsfsdfdh</div>
+            <div className={style.OurTeamBox}>
+                <div className={style.ourTeamHeader}>
+                    <h3>Our Team</h3>
+                    <div className={style.btns}>
+                        <div className={style.Arrowbtn}>-</div>
+                        <div className={style.Arrowbtn}>-</div>
+                    </div>
+                </div>
+                <div className={style.outTeamContent}>
+                    <div className={style.contentBox} >
+                        <img src={image1} width={203} height={202} alt="" srcset="" />
+                        <div className={style.ourTeamInfo} >
+                            <h3>Krishna</h3>
+                            <p className={style.subHeading} >CEO, Wipro</p>
+                            <p className={style.contentPara} >Immerse yourself in the calming ambience as we curate an oasis for your mind, body, and soul. We believe in the power of simplicity, and our spa reflects this philosophy. Every detail, from the soothing decor to the user-friendly online experience, is designed to enhance your wellness journey.</p>
+                        </div>
+                    </div>
+                    <div className={style.contentBox} >
+                        <img src={image1} width={203} height={202} alt="" srcset="" />
+                        <div className={style.ourTeamInfo} >
+                            <h3>Krishna</h3>
+                            <p className={style.subHeading}>CEO, Wipro</p>
+                            <p className={style.contentPara} >Immerse yourself in the calming ambience as we curate an oasis for your mind, body, and soul. We believe in the power of simplicity, and our spa reflects this philosophy. Every detail, from the soothing decor to the user-friendly online experience, is designed to enhance your wellness journey.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={style.ourVisionBox} >
+                <div className={style.visionContentBox} >
+                    <img src={image2} alt="" srcset="" />
+                    <div className={style.visionText} >
+                        <h3>Our Vision</h3>
+                        <p>Immerse yourself in the calming ambience as we curate an oasis for your mind, body, and soul. We believe in the power of simplicity, and our spa reflects this philosophy. Every detail, from the soothing decor to the user-friendly online experience, is designed to enhance your wellness journey.Immerse yourself in the calming ambience as we curate an oasis for your mind, body, and soul. </p>
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
--- src/layouts/AboutUsPageLayout/AboutUsPage.js
+++ src/layouts/AboutUsPageLayout/AboutUsPage.js
@@ -2,14 +2,15 @@ import React from 'react';
 import AboutusNavBar from '../../components/Aboutus/AboutUsNavBar/AboutusNavBar';
 import AboutUsDetails from '../../components/Aboutus/AboutUsDetails/AboutUsDetails';
 import Footer from '../../components/Footer/Footer';
+import style from './AboutUsPage.module.css';
 
 function AboutUsPage() {
   return (
-    <>
+    <div className={style.container}>
     <AboutusNavBar/>
     <AboutUsDetails/>
       {/* <Footer/> */}
-    </>
+    </div>
   )
 }
 
