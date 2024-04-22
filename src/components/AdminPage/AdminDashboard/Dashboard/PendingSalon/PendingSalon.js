7ad99b1cedfc5fb8b4a72aed76f206ed3bf47172
mahmudur987 <safemahmud987@agmail.com>
2024-04-22 18:56:29 +0600

    add pending salonn section

--- src/components/AdminPage/AdminDashboard/Dashboard/PendingSalon/PendingSalon.js
+++ src/components/AdminPage/AdminDashboard/Dashboard/PendingSalon/PendingSalon.js
@@ -1,8 +1,15 @@
 import React from "react";
 import styles from "./PendingSalon.module.css";
 import img from "../../../../../assets/images/SalonDetail/slide4.png";
+import { Link } from "react-router-dom";
 const PendingSalon = () => {
   const pendingSalonData = [
+    {
+      image: img,
+      name: "Getanjali Salon",
+      address: "Windmills Road ,Bengaluru",
+      date: "12,Mar ,2024",
+    },
     {
       image: img,
       name: "Getanjali Salon",
@@ -24,22 +31,28 @@ const PendingSalon = () => {
   ];
 
   return (
-    <section>
-      <div>
+    <section className={styles.mainContainer}>
+      <div className={styles.top}>
+        <h2>Pending Salon (11)</h2>
+        <Link to={"#"}>view all</Link>
+      </div>
+
+      <div className={styles.contents}>
         {pendingSalonData?.slice(0, 4).map((x, y) => (
-          <div key={y}>
+          <div key={y} className={styles.card}>
             <figure>
               <img src={x.image} alt="" />
             </figure>
 
-            <div>
-              <h3>{x.name}</h3>
-              <p>{x.address}</p>
-              <p>Applied on {x.date}</p>
-
-              <div>
-                <button>Approve</button>
-                <button>Reject</button>
+            <div className={styles.cardBottom}>
+              <div className={styles.info}>
+                <h3>{x.name}</h3>
+                <p className={styles.address}>{x.address}</p>
+                <p className={styles.date}>Applied on {x.date}</p>
+              </div>
+              <div className={styles.cardAction}>
+                <button className={styles.approve}>Approve</button>
+                <button className={styles.reject}>Reject</button>
               </div>
             </div>
           </div>
--- src/pages/AdminPages/Dashboard/AdminDashboard.js
+++ src/pages/AdminPages/Dashboard/AdminDashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./AdminDashboard.module.css";
 import Header from "../../../components/AdminPage/AdminDashboard/Dashboard/Header/Header";
+import PendingSalon from "../../../components/AdminPage/AdminDashboard/Dashboard/PendingSalon/PendingSalon";
 const AdminDashboard = () => {
   return (
     <main className={styles.mainContainer}>
       <Header />
+      <PendingSalon />
     </main>
   );
 };
