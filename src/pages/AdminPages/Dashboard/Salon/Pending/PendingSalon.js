7fcaf314b1aa6e86df7d2edf07f05e17baff0866
mahmudur987 <safemahmud987@agmail.com>
2024-04-26 16:37:21 +0600

    complete pending salon

--- src/pages/AdminPages/Dashboard/Salon/Pending/PendingSalon.js
+++ src/pages/AdminPages/Dashboard/Salon/Pending/PendingSalon.js
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import SalonInDashBoard from "../../../../../layouts/Admin/SalonInDashboard/SalonInDashBoard";
-
+import FilterSection from "../../../../../components/AdminPage/AdminDashboard/Salon/Pending/FilterSection/FilterSection";
+import styles from "./PendingSalon.module.css";
+import PendingAllSalon from "../../../../../components/AdminPage/AdminDashboard/Salon/Pending/SingleSalon/PendingAllSalon";
+import img from "../../../../../assets/images/SalonDetail/slide4.png";
+const pendingSalonData = [
+  {
+    id: 1,
+    image: img,
+    name: "Getanjali Salon",
+    address: "Windmills Road ,Bengaluru",
+    date: "12,Mar ,2024",
+    ownerName: "fajila khatun ",
+  },
+  {
+    id: 2,
+    image: img,
+    name: "Getanjali Salon",
+    address: "Windmills Road ,Bengaluru",
+    date: "12,Mar ,2024",
+    ownerName: "fajila khatun ",
+  },
+  {
+    id: 3,
+    image: img,
+    name: "Getanjali Salon",
+    address: "Windmills Road ,Bengaluru",
+    date: "12,Mar ,2024",
+    ownerName: "fajila khatun ",
+  },
+  {
+    id: 4,
+    image: img,
+    name: "Getanjali Salon",
+    address: "Windmills Road ,Bengaluru",
+    date: "12,Mar ,2024",
+    ownerName: "fajila khatun ",
+  },
+];
 const PendingSalon = () => {
+  const [viewBy, setViewBy] = useState(true);
+  const [selectedSalon, setSelectedSalon] = useState([]);
+
   return (
     <SalonInDashBoard>
-      <div>pending</div>
+      <section className={styles.mainContainer}>
+        <FilterSection
+          viewBy={viewBy}
+          setViewBy={setViewBy}
+          selectedSalon={selectedSalon}
+        />
+
+        <PendingAllSalon
+          selectedSalon={selectedSalon}
+          setSelectedSalon={setSelectedSalon}
+          pendingSalonData={pendingSalonData}
+          viewBy={viewBy}
+        />
+      </section>
     </SalonInDashBoard>
   );
 };
