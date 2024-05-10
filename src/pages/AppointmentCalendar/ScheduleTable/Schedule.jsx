7c7fec6190361d3943a41d1f00e8629f54574d47
Krishna-Kant-Patel <cu.17bcs1796@gmail.com>
2024-05-10 11:55:11 +0530

    :fire:

--- src/pages/AppointmentCalendar/ScheduleTable/Schedule.jsx
+++ src/pages/AppointmentCalendar/ScheduleTable/Schedule.jsx
@@ -1,4 +1,4 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import style from './schedule.module.css';
 import { GetCalenderdata } from '../../../services/calender';
 
@@ -6,82 +6,7 @@ import { GetCalenderdata } from '../../../services/calender';
 const ScheduleTable = () => {
   // Define state variables
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [profiles, setPersons] = useState([{
-    name: "first",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "krishna",
-    image: "ddhfkjh"
-  },
-  {
-    name: "last",
-    image: "ddhfkjh"
-  }]);
+  const [profiles, setEmployee] = useState(null);
   const [timeDuration, setDurations] = useState([{
     time: "6:00 AM"
   },
@@ -102,16 +27,21 @@ const ScheduleTable = () => {
   {
     time: "6:00 AM"
   }])
-  
-// useEffect( async ()=>{
-// //  const {res, err} = await GetCalenderdata()
-// //  if(res){
-// //   console.log(res)
-// //  }
-// //  else{
-// //   console.log(err);
-// //  }
-// },[])
+
+  useEffect(() => {
+    const getdata = async () => {
+      const { res, err } = await GetCalenderdata()
+      if (res) {
+        setEmployee(res.data);
+        console.log(res)
+      }
+      else {
+        console.log(err);
+      }
+    }
+    getdata()
+    console.log(profiles);
+  }, [])
 
 
 
@@ -127,7 +57,7 @@ const ScheduleTable = () => {
     box.scrollLeft = box.scrollLeft - 170;
   };
   return (<>
-  <div className={style.durationsBox}>
+    <div className={style.durationsBox}>
       {timeDuration &&
         timeDuration.map((ele) => <p>{ele.time}</p>)}
 
@@ -135,22 +65,53 @@ const ScheduleTable = () => {
     <div className={style.header} >
       <button className={style.prev} onClick={nextProfile}>&#10094;</button>
       <div className={style.carousel} id='header'>
-        {profiles.map((profile, index) => (<div className={style.profileContainer} >
-          <div key={index} className={style.profileBox}>
-            <img width={41} height={24} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSasIydkiFM8Nmx5KTx2iAshCaV_YINqfc5TBrUhN9KA&s" alt="" />
-            <p>{profile.name}{index}</p>
-          </div>
-          <div className={style.slides} >{index}
+        {profiles &&
+          profiles.map((profile, index) => (
+            <div className={style.profileContainer} >
+              <div key={index} className={style.profileBox}>
+                <img src={profile.stylistImage?.public_url} alt="profile image" />
+                <p>{profile.stylistName}</p>
+              </div>
+              <div className={style.slides} >
+                {profile.appointments.map((ele) => {
+                  return <>
+                    {ele.services.map((item) => {
+                      return <>
+                        <div className={style.appointmentBox} style={{
+                          width: 160,
+                          height: 200,
+                          backgroundColor: `${ele.color}`
+                        }} >
+                          <div className={style.clientDetailsBox} >
 
-          </div></div>
-        ))}
+                            <div>
+                              <p className={style.timeDurations} >{item.time_takenby_service}</p>
+                              <p className={style.serviceNames} >{item.service_name}</p>
+                              <p className={style.clientNames} >{ele.ClientName}</p>
+                            </div>
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24">
+                              <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M12 6h.01M12 12h.01M12 18h.01" />
+                            </svg>
+
+                          </div>
+                        </div></>
+
+                    })}
+
+                  </>
+
+                })}
+
+
+              </div></div>
+          ))}
       </div>
 
 
       <button className={style.next} onClick={prevProfile} >&#10095;</button>
     </div>
-    
-    
+
+
   </>
   );
 };
--- src/services/calender.js
+++ src/services/calender.js
@@ -4,7 +4,7 @@ export const GetCalenderdata = async () => {
    
       try {
         const headers = {
-          token: localStorage.getItem("jwtToken"),
+          token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1M2EzOWI2NmI5NjM4OWFmOTIzN2U4YyIsImlhdCI6MTcxNTMxNDM3NiwiZXhwIjoxNzE1NzQ2Mzc2fQ._doud6nKNEMH8ybl20jkIi6FBsRFbybLqkKX-kLXBEM',
         };
         const res = await axiosInstance.get(`https://backend.treato.in/api/v1/reports/calenderReport?date=2024-03-10`, { headers });
     
