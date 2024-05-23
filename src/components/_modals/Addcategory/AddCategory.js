e260c21f4dc3479e354b73e05c052c4af24795be
mahmudur987 <safemahmud987@agmail.com>
2024-05-23 20:26:53 +0600

    update color select

--- src/components/_modals/Addcategory/AddCategory.js
+++ src/components/_modals/Addcategory/AddCategory.js
@@ -138,23 +138,6 @@ const AddCategory = ({ showModal, onClose }) => {
 
             <div className={styles.selectWrapper}>
               <ColorSelect />
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                >
-                  <path
-                    d="M6 9L12 15L18 9"
-                    stroke="black"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </span>
             </div>
           </div>
 
