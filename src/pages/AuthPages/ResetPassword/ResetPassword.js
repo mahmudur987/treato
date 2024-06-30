fd1bc09ce97df5ebba7a9bc4ce80ff0e42faf512
mahmudur987 <108387570+mahmudur987@users.noreply.github.com>
2024-06-30 21:34:17 +0600

    add new screen on reset password page

--- src/pages/AuthPages/ResetPassword/ResetPassword.js
+++ src/pages/AuthPages/ResetPassword/ResetPassword.js
@@ -3,11 +3,12 @@ import AuthPage from "../../../layouts/AuthPageLayout/AuthPage";
 import PrimaryButton from "../../../components/Buttons/PrimaryButton/PrimaryButton";
 import styles from "./ResetPassword.module.css";
 import { eyeline } from "../../../assets/images/icons";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { resetPassword } from "../../../services/auth";
 import { toast } from "react-toastify";
-
+import img from "../../../assets/icons/Success Circle.png";
+import LoadSpinner from "../../../components/LoadSpinner/LoadSpinner";
 const ResetPassword = () => {
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -15,6 +16,8 @@ const ResetPassword = () => {
   const [showNewPassword, setShowNewPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [errorMEssage, seterrorMEssage] = useState("");
+  const [complete, setComplete] = useState(false);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   useEffect(() => {
     // Parse the URL to extract the id and token
@@ -39,12 +42,16 @@ const ResetPassword = () => {
       // Passwords match, now validate against the regex pattern
       if (passwordRegex.test(newPassword)) {
         // Password is valid, proceed to save
+
+        setLoading(true);
+
         resetPassword(paramsInfo.id, paramsInfo.token, confirmPassword).then(
           (res) => {
             console.log(res);
             if (res.success === true) {
-              navigate("/login");
+              setComplete(true);
               toast.success("Password updated successfully");
+              setLoading(false);
             } else if (res.success === false) {
               console.log(res.error.response.data.message);
               seterrorMEssage(res.error.response.data.message);
@@ -69,58 +76,83 @@ const ResetPassword = () => {
     setShowConfirmPassword(!showConfirmPassword);
   };
 
+  if (loading) {
+    return <LoadSpinner />;
+  }
+
   return (
     <AuthPage>
-      <div className={styles.ResetScreen}>
-        <h1>Reset your password</h1>
-        <h3>
-          Set a new password for your registered account{" "}
-          <span>{localStorage?.getItem("userEmail")}</span>
-        </h3>
-        <div className={styles.inputGroup}>
-          <label htmlFor="newPassword">New Password</label>
-          <div className={styles.inputWrap}>
-            <input
-              type={showNewPassword ? "text" : "password"}
-              id="newPassword"
-              placeholder="New Password"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
-            />
-            <img
-              src={eyeline}
-              alt="eyeIcon"
-              className={styles.eyeIcon}
-              onClick={toggleNewPasswordVisibility}
-            />
+      {!complete && (
+        <div className={styles.ResetScreen}>
+          <h1>Reset your password</h1>
+          <h3>
+            Set a new password for your registered account{" "}
+            <span>{localStorage?.getItem("userEmail")}</span>
+          </h3>
+          <div className={styles.inputGroup}>
+            <label htmlFor="newPassword">New Password</label>
+            <div className={styles.inputWrap}>
+              <input
+                type={showNewPassword ? "text" : "password"}
+                id="newPassword"
+                placeholder="New Password"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+              <img
+                src={eyeline}
+                alt="eyeIcon"
+                className={styles.eyeIcon}
+                onClick={toggleNewPasswordVisibility}
+              />
+            </div>
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <div className={styles.inputWrap}>
+              <input
+                type={showConfirmPassword ? "text" : "password"}
+                id="confirmPassword"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <img
+                src={eyeline}
+                alt="eyeIcon"
+                className={styles.eyeIcon}
+                onClick={toggleConfirmPasswordVisibility}
+              />
+            </div>
+          </div>
+          <div className={styles.error}>{errorMEssage}</div>
+          <PrimaryButton
+            className={styles.submitBtn}
+            onClick={handleSavePassword}
+          >
+            Save Password
+          </PrimaryButton>
+        </div>
+      )}
+
+      {complete && (
+        <div className={styles.container}>
+          <div className={styles.wrapper}>
+            <h2>Password reset successful</h2>
+            <figure>
+              <img src={img} alt="im" />
+            </figure>
+            <p>
+              {" "}
+              You have successfully reset your Treato password. Click below to
+              sign in with your new password.{" "}
+            </p>
+            <Link to={"/login"}>
+              <button>Sign in</button>
+            </Link>
           </div>
         </div>
-        <div className={styles.inputGroup}>
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <div className={styles.inputWrap}>
-            <input
-              type={showConfirmPassword ? "text" : "password"}
-              id="confirmPassword"
-              placeholder="Confirm Password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            <img
-              src={eyeline}
-              alt="eyeIcon"
-              className={styles.eyeIcon}
-              onClick={toggleConfirmPasswordVisibility}
-            />
-          </div>
-        </div>
-        <div className={styles.error}>{errorMEssage}</div>
-        <PrimaryButton
-          className={styles.submitBtn}
-          onClick={handleSavePassword}
-        >
-          Save Password
-        </PrimaryButton>
-      </div>
+      )}
     </AuthPage>
   );
 };
