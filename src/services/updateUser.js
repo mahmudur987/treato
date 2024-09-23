fb4730fc21cc3ae5c34a74901e9ff5d104d9bc7d
mahmudur987 <safemahmud987@gmail.com>
2024-09-23 17:18:55 +0600

    update profile setting

--- src/services/updateUser.js
+++ src/services/updateUser.js
@@ -3,13 +3,12 @@ import axiosInstance from "./axios";
 /** Register the user after Verifying the OTP `POST: /api/auth/register/` */
 export const updateUser = async (userJWT, data) => {
   try {
-    console.log("updateUser data", data);
     const res = await axiosInstance.patch(`profile/update`, data, {
       headers: {
         token: userJWT,
       },
     });
-    console.log(res);
+
     return { res: res, err: null };
   } catch (error) {
     return { err: error, res: null };
