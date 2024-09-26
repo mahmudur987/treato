f222eb341fc83e956b8eccda24b0852b2e70069e
Krishna <cu.17bcs1796@gmail.com>
2024-09-26 11:57:34 +0530

    :bug: lazy loading applied.

--- src/layouts/AboutUsPageLayout/AboutUsPage.js
+++ src/layouts/AboutUsPageLayout/AboutUsPage.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import AboutUsDetails from '../../pages/Aboutus/AboutUsDetails/AboutUsDetails';
+import React, { Suspense } from 'react';
+// import AboutUsDetails from '../../pages/Aboutus/AboutUsDetails/AboutUsDetails';
 import Footer from '../../components/Footer/Footer'
 import style from './AboutUsPage.module.css';
 import Navbar from '../../components/Navbar/Navbar'
+import LoadSpinner from '../../components/LoadSpinner/LoadSpinner';
+const AboutUsDetails = React.lazy(() => import('../../pages/Aboutus/AboutUsDetails/AboutUsDetails'));
 
 function AboutUsPage() {
   return (
     <div className={style.container}>
     <Navbar/>
+    <Suspense fallback={<LoadSpinner/>} >
     <AboutUsDetails/>
+    </Suspense>
       <Footer/>
     </div>
   )
