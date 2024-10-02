8576b3e5882d9564222f91600ee084f5467152b8
mahmudur987 <108387570+mahmudur987@users.noreply.github.com>
2024-10-02 22:48:49 +0600

    Update TeamManageMent.jsx

--- src/pages/partnerPages/Team/TeamData/TeamManageMent.jsx
+++ src/pages/partnerPages/Team/TeamData/TeamManageMent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import TeamDetails from "./TeamDetails";
 import TimeSchedule from "./TimeSchedule";
 // import sty from "./TeamManageMent.module.css";
